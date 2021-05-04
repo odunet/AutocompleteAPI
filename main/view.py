58065921414f5d158f72cfa994a084772a76ddf9
odunet <odunet2000@yahoo.com>
2021-05-04 17:42:44 +0100

    Updated views.py

--- main/view.py
+++ main/view.py
@@ -24,16 +24,38 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/api/<key>', methods=["POST", "GET"])
+@app.route('/api/<key>', methods=["GET"])
 @cross_origin()
-def searchB(key):
+def search(key):
+    """
+        Parameters
+        ----------
+        key : str
+            A key are arguments send with the url "GET" request from the client.
+
+        Request Type
+        -------
+        methods = GET
+
+
+        Returns
+        -------
+        list
+            The list contains strings of predicted autocomplete items e.g.:
+            ['oluwatosin', 'oluwaseun', 'oluwasegun', 'oluwakemi', 'olumide', 'olufunso',
+            'olufemi', 'oluremi', 'olushola']
+    """
     if not key:
         return "No key included"
     else:
+        # Create an instance of the Tries class and append data
+        # Note that in practise, the data in required structure will be handly for increased speed.
         testTrie = Tries()
         for i in range(len(data)):
             testTrie[data[i]] = data[i]
         result = testTrie.autoSearch(key)
+
+        # Handle result of search and send to server.
         if result[0] == False:
             print({'Error': 'No Result Found'})
             return jsonify([])
@@ -42,13 +64,3 @@ def searchB(key):
             for index, i in enumerate((result[1])):
                 jsonData.append(i)
             return jsonify(jsonData)
-
-# @app.route('/api/<key>', methods=["POST", "GET"])
-# def searchA(key):
-#     if not key:
-#         return "No key included"
-#     else:
-#         testTrie = Tries()
-#         for i in range(len(data)):
-#             testTrie[data[i]] = data[i]
-#         return str(testTrie.autoSearch(key))
